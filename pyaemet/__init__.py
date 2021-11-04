282827f74ddf63216d1c0d1147b481ea00c2887d
Jaimedgp <jaime.diez.gp@gmail.com>
2021-11-04 11:38:46 +0100

    add package information

--- pyaemet/__init__.py
+++ pyaemet/__init__.py
@@ -1,2 +1,12 @@
-from pyaemet.AEMET import *
+""" Docstring """
 
+__author__ = "Jaime Diez Gonzalez-Pardo"
+__date__ = "November, 2021"
+
+__license__ = "GPL"
+__version__ = "1.0.1"
+__maintainer__ = "Jaimedgp"
+__email__ = "jaime.diez.gp@gmail.com"
+
+__dataset_doi__ = "10.5281/zenodo.5642979"
+__version_doi__ = ""
