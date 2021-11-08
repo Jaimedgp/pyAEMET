056282f3a3de1a863c772d2326e88371ad9cd9e1
Jaimedgp <jaime.diez.gp@gmail.com>
2021-11-08 19:24:22 +0100

    Update

--- pyaemet/__init__.py
+++ pyaemet/__init__.py
@@ -1,14 +1,15 @@
 """ Docstring """
 
 __author__ = "Jaime Diez Gonzalez-Pardo"
+__author_orcid__ = "0000-0001-7268-9933"
 __date__ = "November, 2021"
 
-__license__ = "GPL"
-__version__ = "0.0.1"
+__license__ = "GPL-3.0 License"
+__version__ = "1.0.1"
 __maintainer__ = "Jaimedgp"
 __email__ = "jaime.diez.gp@gmail.com"
 
+__repository__ = "https://github.com/Jaimedgp/pyAEMET"
 __dataset_doi__ = "10.5281/zenodo.5642979"
-__version_doi__ = ""
 
 from pyaemet.valores_climatologicos import AemetClima
